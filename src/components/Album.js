0282a78bbf745e787499e42facfa666c8af96fef
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2020-01-05 19:21:17 +0530

    removed sorted photos

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,21 +3,11 @@ import axios from 'axios';
 
 function Album() {
 const [photos, setPhotos]=useState([]);
-const [sortedPhotos, setSortedPhotos]=useState({})
 useEffect(()=>{
-    axios.get('https://api.unsplash.com/photos/random?count=30&client_id=eabab3af0950e1039a56eac39d421af54bf8c9156b61333cbe93c7160c768d6d').then((response)=>{
-    console.log(response)    
+    axios.get('https://api.unsplash.com/photos/random?count=30&client_id=eabab3af0950e1039a56eac39d421af54bf8c9156b61333cbe93c7160c768d6d').then((response)=>{    
     response['data'].map((image)=>{
             const {alt_description, urls, created_at}=image;
             const imageYear=new Date(image.created_at).getFullYear();
-            console.log("here1",imageYear in sortedPhotos )
-            if(imageYear in sortedPhotos){
-                console.log("here")
-                setSortedPhotos(sortedPhotos=>({...sortedPhotos, [imageYear]:[...sortedPhotos.imageYear, image]}))
-            }
-            else{
-                setSortedPhotos(sortedPhotos=>({...sortedPhotos, [imageYear]:[image]}))
-            }
             setPhotos(prevState=>[...prevState, image])
             setPhotos(prevState=>prevState.sort(function(a,b){
                 return new Date(a.created_at)-new Date(b.created_at)
@@ -29,12 +19,6 @@ useEffect(()=>{
 
   return (
     <div className="image-container">
-        {Object.keys(sortedPhotos).length?(
-            Object.keys(sortedPhotos).map((year)=>(
-            <h3>{year}</h3>
-            {sortedPhotos.year.map((image)=><img className="image-view" src={image.urls.small} alt={image.alt_description} />))}
-            ))
-        ):null}
       {photos.length?photos.map((image)=><img className="image-view" src={image.urls.small} alt={image.alt_description} />):null}
     </div>
   );
