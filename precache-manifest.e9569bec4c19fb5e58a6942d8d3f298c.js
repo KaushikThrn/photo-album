577f259e94981d743d8c564ef7685615363ed64a
Kaushik Thiyagarajan <thrn.kaushik@gmail.com>
2020-01-12 02:25:42 +0530

    Updates

---
+++ precache-manifest.e9569bec4c19fb5e58a6942d8d3f298c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8009d52c521fc91336ec0f4f140b7d87",
+    "url": "/photo-album/index.html"
+  },
+  {
+    "revision": "710853f5836994ad0aee",
+    "url": "/photo-album/static/css/main.2d20917d.chunk.css"
+  },
+  {
+    "revision": "09cc18670c270df3d259",
+    "url": "/photo-album/static/js/2.ec2ffcdf.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/photo-album/static/js/2.ec2ffcdf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "710853f5836994ad0aee",
+    "url": "/photo-album/static/js/main.a563477e.chunk.js"
+  },
+  {
+    "revision": "ae97bae6013fd48cf705",
+    "url": "/photo-album/static/js/runtime-main.b89f4ae2.js"
+  },
+  {
+    "revision": "d121836307269255c78c258e46f0add4",
+    "url": "/photo-album/static/media/album-icon.d1218363.svg"
+  },
+  {
+    "revision": "478887b36f9b990f79ee9dbdb5bb2a88",
+    "url": "/photo-album/static/media/create.478887b3.svg"
+  },
+  {
+    "revision": "c195fcb7dbef9b9d1c9c757050716d4f",
+    "url": "/photo-album/static/media/photo-icon.c195fcb7.svg"
+  },
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/photo-album/static/media/search.a87eca9e.svg"
+  },
+  {
+    "revision": "0060a70466bb12f775466746c64c8966",
+    "url": "/photo-album/static/media/share-icon.0060a704.svg"
+  },
+  {
+    "revision": "04dcba43ad0fdbcdc4280f3942490b42",
+    "url": "/photo-album/static/media/upload.04dcba43.svg"
+  }
+]);
